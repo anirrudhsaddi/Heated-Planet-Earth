83859ae181a61261de7adc5aeadb597b9e408ed5
DaHornNPT <dahornnpt@gmail.com>
2014-11-16 12:36:16 -0500

    Added Monitor class
    used to monitor the current time in simulation and stop the simulation
    when done

---
+++ src/common/Monitor.java
@@ -0,0 +1,42 @@
+package common;
+
+import messaging.Message;
+import messaging.MessageListener;
+import messaging.Publisher;
+import messaging.events.StartMessage;
+import messaging.events.StopMessage;
+
+public class Monitor implements IMonitorCallback,MessageListener{
+	
+	IMonitorCallback callback;
+	int currentTimeInSimulation;
+	int simulationLength;	// in months
+	int timeStep;			// in minutes
+	
+	Publisher publisher;
+	
+	public Monitor(){
+		
+		Publisher publisher = Publisher.getInstance();
+		publisher.subscribe(StartMessage.class, this);
+	}
+	
+	public void onMessage(Message msg) {
+		if (msg instanceof StartMessage) {
+			StartMessage startMsg = (StartMessage)msg;
+			this.simulationLength = startMsg.simulationLength();
+			this.timeStep = startMsg.timeStep();
+			
+			this.currentTimeInSimulation = 0;
+		}
+	}
+
+	@Override
+	public void notifyCurrentInterval(int currSimulationInterval) {
+		this.currentTimeInSimulation = currSimulationInterval*this.timeStep;
+		
+		if(this.currentTimeInSimulation >= simulationLength){
+			publisher.send(new StopMessage());
+		}
+	}
+}
