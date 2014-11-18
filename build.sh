708d6ffde4e8ab9ac15c06c933b8e606b7a826b6
Brandon Webb <webb.c.brandon@gmail.com>
2014-11-17 23:00:20 -0500

    added neo4j

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 find src -name "*.java" > sources.txt
 
 # include the embedded neo4j
-javac -source 1.6 -d bin -cp bin @sources.txt
+javac -target 1.6 -d bin -cp "resource/neo4j/*.jar;bin" @sources.txt
