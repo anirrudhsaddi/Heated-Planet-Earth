6add01f4b96260dfa70bd7887264ecaa67d95b1a
gooftroop <webb.c.brandon@gmail.com>
2014-11-19 13:30:21 -0500

    implemented ResultMessage

--- src/messaging/events/ResultMessage.java
+++ src/messaging/events/ResultMessage.java
@@ -1,28 +1,43 @@
 package messaging.events;
 
-import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 import messaging.Message;
 
 public class ResultMessage implements Message {
 	
-	private ResultSet resultSet;
+	private int width = 10;
 	private boolean needsCalculation;
 	
-	public ResultMessage(ResultSet resultSet, boolean needsCalculation) {
-		
-		if (resultSet == null)
-			throw new IllegalArgumentException("Invalid resultSet provided");
-		
-		this.resultSet = resultSet;
-		this.needsCalculation = needsCalculation;
-	}
+	private final Map<Integer, Double> grid;
 	
-	public ResultSet getResultSet() {
-		return this.resultSet;
+	public ResultMessage(boolean needsCalculation) {
+		
+		this.needsCalculation = needsCalculation;
+		
+		grid = new TreeMap<Integer, Double>();
 	}
 	
 	public boolean needsCalculation() {
 		return this.needsCalculation;
+	}
+	
+	public boolean hasTemperature(int longitude, int latitude) {
+		return grid.containsKey(latitude * width + longitude);
+	}
+	
+	public void setTemperature(int longitude, int latitude, double temp) {
+		if (longitude < 0 || latitude < 0)
+			throw new IllegalArgumentException("index (" +longitude + ", " +latitude+ ") out of bounds");
+		
+		grid.put(latitude * width + longitude, temp);
+	}
+	
+	public double getTemperature(int longitude, int latitude) {
+		if (longitude < 0 || latitude < 0)
+			throw new IllegalArgumentException("index (" + longitude + ", " + latitude + ") out of bounds");
+		
+		return grid.get(latitude * width + longitude);
 	}
 }
