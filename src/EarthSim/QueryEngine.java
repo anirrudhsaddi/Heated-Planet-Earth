c5dc55f81ef96c81219899e35ee7ee66458aca80
chiqua25 <juwujaren@gmail.com>
2014-11-19 20:07:48 -0500

    added QueryEngine method to getQueryValues()

--- src/EarthSim/QueryEngine.java
+++ src/EarthSim/QueryEngine.java
@@ -1,6 +1,29 @@
 package EarthSim;
 
+import EarthSim.widgets.QueryWidget;
+import org.apache.james.mime4j.field.datetime.DateTime;
+
+import javax.management.Query;
+
 public class QueryEngine {
 
-	//Todo: Check with Janice on the code for this class.
+    QueryWidget q = new QueryWidget();
+
+    //TODO: Create method to accept user input from QueryWidgets
+    private void getQueryValues(String simName, float axialTilt, float eccentricity, DateTime startTime, DateTime endTime, double wLat, double eLat, double nLat, double sLat ) {
+
+        this.simName = simName;
+        this.axialTilt = axialTilt;
+        this.eccentricity = eccentricity;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.wLat = wLat;
+        this.eLat = eLat;
+        this.sLat = sLat;
+        this.nLat = nLat;
+
+    }
+
+    //TODO: validate input to make sure that are valid
+
 }
