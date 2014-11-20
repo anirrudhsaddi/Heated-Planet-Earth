aba5d43829c2c794be8eefd985bc8bd7d71895a8
Anirrudh Saddi <asaddi3@mail.gatech.edu>
2014-11-19 19:46:58 -0500

    Replacing my previous commit

--- src/common/Monitor.java
+++ src/common/Monitor.java
@@ -36,18 +36,8 @@ public class Monitor implements IMonitorCallback, MessageListener{
 	@Override
 	public void notifyCurrentInterval(long dateTime) {
 		
-		
-		//Todo: talk to Brandon
-	/*	this.currentTimeInSimulation = currSimulationInterval * this.timeStep;
-=======
 		// dateTime will be the millisecond representation of the current date and time from Jan. 4th, 1970 (epoch) - Constants.START_DATE is our start date. If you use 
 		// that object, call .clone() on it first
->>>>>>> branch 'master' of https://github.com/gooftroop/CS6310P3.git
-		
-<<<<<<< HEAD
-		if(this.currentTimeInSimulation >= simulationLength){
-			publisher.send(new StopMessage());
-			}*/
 		
 		// From here you can generate a Calendar object and use it to check the current length from START_DATE in terms of months. If it's longer than simulationLength,
 		// you can also check the current time. Since the startTime passed in from StartMessage will be 0 on normal simulations, and START_MESSAGE starts at midnight,
